698d604ea5da087924b0d5bf5dc85013e604d83d
Luca Braun <l.braun@rto.de>
2018-02-09 14:15:17 +0100

    Finished DB switch

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const {Wrapper} = require(`${__dirname}/lib/Wrapper`),
   {Global} = require(`${__dirname}/lib/Global`),
   {BaseModule} = require(`${__dirname}/lib/BaseModule`),
   {Output} = require(`${__dirname}/lib/Output`),
-  {DB} = require(`${__dirname}/lib/DB`),
   {Helper} = require(`${__dirname}/lib/Helper`),
-  {DelayedSave} = require(`${__dirname}/lib/DelayedSave`),
-  {Formatter} = require(`${__dirname}/lib/Formatter`);
+  {Formatter} = require(`${__dirname}/lib/Formatter`),
+  {Parser} = require(`${__dirname}/lib/Parser`);
 
 // Export all modules.
 exports.WebCrawler = Wrapper;
 exports.Global = Global;
 exports.BaseModule = BaseModule;
 exports.Output = Output;
-exports.DB = new DB();
 exports.Helper = Helper;
-exports.DelayedSave = DelayedSave;
 exports.Formatter = Formatter;
+exports.Parser = Parser;
