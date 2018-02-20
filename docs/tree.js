3fdd824580f3562e3d8c28ef021e5a923bcbba26
Luca Braun <l.braun@rto.de>
2018-02-20 16:26:24 +0100

    Add docs directory

---
+++ docs/tree.js
@@ -0,0 +1,83 @@
+var tree = [
+	["Global", "global.html", "", [
+		[
+       	"BaseModule", 
+	"BaseModule.html", "", [
+  
+	]
+]
+,[
+       	"Crawler", 
+	"Crawler.html", " < BaseModule", [
+  
+	]
+]
+,[
+       	"DB", 
+	"DB.html", "", [
+  
+	]
+]
+,[
+       	"DelayedSave", 
+	"DelayedSave.html", "", [
+  
+	]
+]
+,[
+       	"Formatter", 
+	"Formatter.html", "", [
+  
+	]
+]
+,[
+       	"Global", 
+	"Global.html", "", [
+  
+	]
+]
+,[
+       	"Helper", 
+	"Helper.html", "", [
+  
+	]
+]
+,[
+       	"MongoDB", 
+	"MongoDB.html", " < BaseModule", [
+  
+	]
+]
+,[
+       	"MySQL", 
+	"MySQL.html", " < BaseModule", [
+  
+	]
+]
+,[
+       	"Output", 
+	"Output.html", " < BaseModule", [
+  
+	]
+]
+,[
+       	"Parser", 
+	"Parser.html", "", [
+  
+	]
+]
+,[
+       	"ScreenShot", 
+	"ScreenShot.html", " < BaseModule", [
+  
+	]
+]
+,[
+       	"Wrapper", 
+	"Wrapper.html", " < BaseModule", [
+  
+	]
+]
+
+	]]
+];
